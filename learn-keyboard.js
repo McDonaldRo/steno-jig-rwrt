70ba2460f4a7206aabe7ee1de541133722e9aacf
McDonaldRo <37886586+McDonaldRo@users.noreply.github.com>
2026-05-08 06:09:54 -0700

    Update learn-keyboard.js

--- learn-keyboard.js
+++ learn-keyboard.js
@@ -88,7 +88,7 @@
 	E["Other chords: G, X, Z, -J"] = IndividualChords(['G-', 'X-', 'Z-', '-J'], 100)
 
 	// RWRT
-	E["RWRT left: X, Z"] = IndividualChords(['X-','Z-'], 100)
-	E["RWRT right: -C, -H, -Q, -V, -W, -X, -Y"] = IndividualChords(['-C', '-H', '-Q', '-V','-W', '-X', '-Y'], 100)
+	E["RWRT left: X, Z"] = IndividualChords(['X-','Z-'])
+	E["RWRT right: -C, -H, -Q, -V, -W, -X, -Y"] = IndividualChords(['-C', '-H', '-Q', '-V','-W', '-X', '-Y'])
 
 })();  // Execute the code in the wrapper function.
